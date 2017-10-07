81d351de30c27e6b5a47366d45ec7f8703c9a111
Nazariy18 <Kereya18@gmail.com>
2017-10-07 13:22:35 +0300

    Update script.js

--- 3d_task1/js/script.js
+++ 3d_task1/js/script.js
@@ -1,34 +1,103 @@
-var i = 0;
+var countScroll = 0, 
+    delta = 0,
+    prevCountScroll = 0;
 
-elem = window;
 
-if (elem.addEventListener) {
-  if ('onwheel' in document) {
-    // IE9+, FF17+, Ch31+
-    elem.addEventListener("wheel", onWheel);
-  } else if ('onmousewheel' in document) {
-    // устаревший вариант события
-    elem.addEventListener("mousewheel", onWheel);
-  } else {
-    // Firefox < 17
-    elem.addEventListener("MozMousePixelScroll", onWheel);
+
+function translateX(number) {
+  var x = 0;
+
+  if( number == 1 || (number - 1)%4 == 0) {
+      x = -2.2;
+  } else if((number + 1)%4 == 0) {
+      x = 2.2;
   }
-} else { // IE8-
-  elem.attachEvent("onmousewheel", onWheel);
+  else if( number%4 == 0) {
+      x = 4.4;
+  }
+
+  return x;
+}
+
+function translateZ(number, leyer) {
+  var z = 0;
+
+  if ((number - leyer) <= 4) {
+      z = 2.2;
+  } else if((number - leyer) > 8 && (number - leyer) <= 12) {
+      z = -2.2;
+  } else if((number - leyer) > 12 && (number - leyer) <= 16) {
+      z = -4.4;
+  }
+
+  return z;
+}
+
+function translateY(number) {
+  var y = 0;
+
+  if ( number > 16 && number < 33) {
+      y = -2.2;
+  } else if(number > 32 && number < 49) {
+      y = -4.4;
+  } else if(number > 48 && number < 65) {
+      y = -6.6;
+  } else if(number > 64 && number < 81) {
+      y = -8.8;
+  } else if(number > 80 && number < 97) {
+      y = -11;
+  }
+
+  return y;
+}
+
+if (window.addEventListener) {
+  if ('onwheel' in document) {
+    window.addEventListener("wheel", onWheel);
+  } else if ('onmousewheel' in document) {
+    window.addEventListener("mousewheel", onWheel);
+  } else {
+    window.addEventListener("MozMousePixelScroll", onWheel);
+  }
+} else { 
+  window.attachEvent("onmousewheel", onWheel);
 }
 
 function onWheel(e) {
   e = e || window.event;
 
+   delta = e.deltaY || e.detail || e.wheelDelta;
+   countScroll += delta/100;
+
+   if (countScroll > 6) {countScroll = 6;}
+   if (countScroll < 0) {countScroll = 0;}
+
+   var numberStart = countScroll * 16 - 15,
+       x = 0,
+       y =0,
+       z =0;
   
-  var delta = e.deltaY || e.detail || e.wheelDelta;
+   for (var i = numberStart; i < numberStart + 16; i++) {
 
-  var info = document.getElementById('delta');
+        if (countScroll >= prevCountScroll && countScroll != 0) {
+            var el = document.getElementsByClassName('box')[i-1];
 
-  i = +i + delta;
+            x = translateX(i);
+            y = translateY(i);
+            z = translateZ(i, numberStart - 1);
+            el.style = "transform: rotateX(-30deg) rotateY(-135deg) translate3d(" + x + "em, " + y + "em, " + z + "em)"; }
+        else {
+            var el = document.getElementsByClassName('box')[i+15];
 
-  alert(i);
+            el.style = "transform: rotateX(-30deg) rotateY(-135deg) translate3d(0em, 0em, 0em)";
+        }
 
+    }
 
+ 
+  prevCountScroll = countScroll;
+
+ 
+    
   e.preventDefault ? e.preventDefault() : (e.returnValue = false);
 }
