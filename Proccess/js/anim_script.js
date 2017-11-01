c5123003fb32a16abcaae000160ef01a39d7364d
Nazariy18 <Kereya18@gmail.com>
2017-11-01 11:18:50 +0200

    Add files via upload

---
+++ Proccess/js/anim_script.js
@@ -0,0 +1,85 @@
+var svg = document.getElementById('Process');
+
+var numberOfSqueares = 1000,
+		squeryRadius = 95,
+		centerX = 104.9,
+		centerY = 103.1;
+
+var rects = [];
+
+var posX = [],
+	  posY = [];
+
+
+
+
+for(var i = 0; i < numberOfSqueares; i++) {
+	    posX.push(Math.random()*squeryRadius);
+	    posY.push(Math.random()*Math.PI*6.14);
+	
+
+	    radius = 102.8 + Math.cos(posY[i])*(posX[i]);
+	    degrees = 101 + Math.sin(posY[i])*(posX[i]);
+
+
+	    rects[i] = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+			rects[i].setAttribute("fill", "hsla(" + anime.random(0, numberOfSqueares) + ", 100%, 50%, 1)");
+			rects[i].setAttribute("x", 102.8);
+			rects[i].setAttribute("y", 101);
+			rects[i].setAttribute("width", "2.8");
+			rects[i].setAttribute("height", "2.8");
+			rects[i].setAttribute("opacity", "0");
+			svg.appendChild(rects[i]);
+}
+
+
+
+function move() {
+
+
+for (var i = 0; i < numberOfSqueares; i++) {
+
+		radius = 103.9 + Math.cos(posY[i])*(posX[i]);
+		degrees = 102.1 + Math.sin(posY[i])*(posX[i]);
+
+		rects[i].setAttribute("x", radius);
+		rects[i].setAttribute("y", degrees);
+
+
+		if (posX[i] < (-1*squeryRadius)) {
+		      	posX[i] += squeryRadius;
+		    	}
+		    posX[i] -= 1.5;
+
+ 
+  }
+
+}
+
+//setInterval(move, 10);
+
+var maxElements = 1000;
+var duration = 2000;
+var radius = 95;
+var distance = 95;
+
+
+var animate = function(el, i) {
+  var angle = Math.random() * Math.PI * 2;
+  anime({
+    targets: el,
+    translateX: [0, Math.cos(angle) * distance],
+    translateY: [0, Math.sin(angle) * distance],
+    opacity: {value: 1, duration: 10},
+    delay: (duration / maxElements) * i,
+    duration: duration,
+    easing: 'linear',
+    loop: true
+  });
+}
+
+
+
+
+
+rects.forEach(animate);
