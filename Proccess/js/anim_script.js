31e13cbe46c3ebd57287e13c7f0f60ab7f30c32e
Nazariy18 <Kereya18@gmail.com>
2017-11-03 16:20:22 +0200

    Update anim_script.js

--- Proccess/js/anim_script.js
+++ Proccess/js/anim_script.js
@@ -1,81 +1,47 @@
-var svg = document.getElementById('Process');
+var svg = document.getElementById('Process'),
+		rectsGroup;
 
-var numberOfSqueares = 250,
+var numberOfSqueares = 500,
 		squeryRadius = 95,
 		centerX = 104.9,
-		centerY = 103.1;
+		centerY = 103.1,
+		duration = 2000,
+		sizeRect = 2.8,
+		rects = [],
+		colors = ['#DC4726', '#46DF60', '#3682F1', '#FEEF35', '#fff'];
 
-var rects = [];
+var countScroll = 0,
+		step = 2;
 
-var posX = [],
-	  posY = [];
+function createSquare(x, y, size) {
+  rectsGroup = document.createElementNS("http://www.w3.org/2000/svg", "g");
+  rectsGroup.setAttribute("id", "rectsGroup");
+  svg.appendChild(rectsGroup);
+  rectsGroup.style.transformOrigin = 'center';
 
-
-
-
-for(var i = 0; i < numberOfSqueares; i++) {
-	    posX.push(Math.random()*squeryRadius);
-	    posY.push(Math.random()*Math.PI*6.14);
-	
-
-	    radius = 102.8 + Math.cos(posY[i])*(posX[i]);
-	    degrees = 101 + Math.sin(posY[i])*(posX[i]);
-
-
+	for(var i = 0; i < numberOfSqueares; i++) {	
 	    rects[i] = document.createElementNS("http://www.w3.org/2000/svg", "rect");
-			rects[i].setAttribute("fill", "hsla(" + anime.random(0, 1000) + ", 100%, 50%, 1)");
-			rects[i].setAttribute("x", 102.8);
-			rects[i].setAttribute("y", 101);
-			rects[i].setAttribute("width", "2.8");
-			rects[i].setAttribute("height", "2.8");
-			rects[i].setAttribute("opacity", "0");
-			svg.appendChild(rects[i]);
+			rects[i].setAttribute("fill", colors[anime.random(0, 4)]);
+			rects[i].setAttribute("x", x - size / 2);
+			rects[i].setAttribute("y", y - size / 2);
+			rects[i].setAttribute("width", "0");
+			rects[i].setAttribute("height", "0");
+			rectsGroup.appendChild(rects[i]);
+		}
 }
 
-
-
-function move() {
-
-
-for (var i = 0; i < numberOfSqueares; i++) {
-
-		radius = 103.9 + Math.cos(posY[i])*(posX[i]);
-		degrees = 102.1 + Math.sin(posY[i])*(posX[i]);
-
-		rects[i].setAttribute("x", radius);
-		rects[i].setAttribute("y", degrees);
-
-
-		if (posX[i] < (-1*squeryRadius)) {
-		      	posX[i] += squeryRadius;
-		    	}
-		    posX[i] -= 1.5;
-
- 
-  }
-
-}
-
-//setInterval(move, 10);
-
-var maxElements = 250;
-var duration = 2000;
-var radius = 95;
-var distance = 95;
-
-
-var animate = function(el, i) {
+var animateSquare = function(el, i) {
   var angle = Math.random() * Math.PI * 2;
-
-  var endX = Math.cos(angle) * distance;
-  var endY = Math.sin(angle) * distance;
-  var delay = duration / maxElements;
+  var endX = Math.cos(angle) * squeryRadius;
+	var endY = Math.sin(angle) * squeryRadius;
+	var delay = duration / numberOfSqueares;
 
   anime({
     targets: el,
     translateX: endX,
     translateY: endY,
-    opacity: {value: 1, duration: 10},
+    width: {value: sizeRect, duration: 10},
+    height: {value: sizeRect, duration: 10},
     delay: delay * i,
     duration: duration,
     easing: 'linear',
@@ -83,8 +49,100 @@ var animate = function(el, i) {
   });
 }
 
+createSquare(centerX, centerY, sizeRect);
+rects.forEach(animateSquare);
+
+
+var Process = anime.timeline({
+  //direction: 'alternate',
+  //loop: true,
+  autoplay: false,
+  update: function(anim) {
+    countScroll = anim.progress;
+  }
+});
+
+Process
+  .add({
+    targets: rectsGroup,
+    easing: 'linear',
+    translateY: [0,  -60],
+    scale: .4,
+    duration: 1000,
+    offset: 0
+  })
+  .add({
+    targets: '#lines polyline',
+    easing: 'linear',
+    strokeDashoffset: [anime.setDashoffset, 0],
+    duration: 1000,
+    offset: 1000
+  })
+  .add({
+    targets: '#lines line',
+    easing: 'linear',
+    strokeDashoffset: [anime.setDashoffset, 0],
+    duration: 500,
+    offset: 1800
+  })
+  .add({
+    targets: "#c1",
+    easing: 'linear',
+    r: [0, 5],
+    duration: 500,
+    offset: 1200
+  })
+  .add({
+    targets: '#c2',
+    easing: 'linear',
+    r: [0, 5],
+    duration: 500,
+    offset: 1500
+  })
+  .add({
+    targets: '#c3',
+    easing: 'linear',
+    r: [0, 5],
+    duration: 500,
+    offset: 1700
+  })
+  .add({
+    targets: '#c4',
+    easing: 'linear',
+    r: [0, 5],
+    duration: 500,
+    offset: 2100
+  })
 
 
 
+function onWheel(e) {
 
-rects.forEach(animate);
+	e = e || window.event;
+  delta = e.deltaY || e.detail || e.wheelDelta;
+  countScroll += delta / 100 * step;
+
+
+  Process.seek(Process.duration * (countScroll / 100));
+
+	e.preventDefault ? e.preventDefault() : (e.returnValue = false);
+
+}
+
+elem = window;
+
+
+if (elem.addEventListener) {
+  if ('onwheel' in document) {
+    // IE9+, FF17+, Ch31+
+    elem.addEventListener("wheel", onWheel);
+  } else if ('onmousewheel' in document) {
+    // устаревший вариант события
+    elem.addEventListener("mousewheel", onWheel);
+  } else {
+    // Firefox < 17
+    elem.addEventListener("MozMousePixelScroll", onWheel);
+  }
+} else { // IE8-
+  elem.attachEvent("onmousewheel", onWheel);
+}
