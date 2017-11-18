cbca83e2bb005e867dcb2c8b822244be82c37603
Nazriy <Kereya18@gmail.com>
2017-11-18 12:01:40 +0200

    Update anim_script.js

--- Proccess/js/anim_script.js
+++ Proccess/js/anim_script.js
@@ -1,28 +1,11 @@
 var yStart = null;
 //var delta = null;
 var yMove = null;
-var elem = document.getElementById('block_animation');
-
-//elem.addEventListener("touchstart", start);
-/*	
-function start(event) {
-	alert("start1: " + event.targetTouches[0].pageY);
-	yStart = event.targetTouches[0].pageY;
-	elem.addEventListener("touchmove", move);
-}
-
-function move(event) {
-	alert("move1: " + event.targetTouches[0].pageY);
-	yMove = event.targetTouches[0].pageY;
-	var delta = yStart - yMove;
-	if(delta > 0) 
-	   {alert("up");}
-	else {alert("down");}
-}*/
+//var elem = document.getElementById('block_animation');
 
 var svg = document.getElementById('Process'),
     svgContainer = document.getElementById('svg_container'),
-    //elem = document.getElementById('block_animation'),
+    elem = document.getElementById('block_animation'),
     rectsGroup = null,
     smallRectsGroup = null,
     rectObl = null,
