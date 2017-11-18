0ac3fd5dc9fe3254d9bd696c4598c6bb44363c1b
Nazriy <Kereya18@gmail.com>
2017-11-18 14:19:10 +0200

    Update anim_script.js

--- Proccess/js/anim_script.js
+++ Proccess/js/anim_script.js
@@ -1,4 +1,4 @@
-alert("14.16");
+alert("14.18");
 
 var yStart = null;
 var yMove = null;
@@ -965,18 +965,13 @@ function scrollAnimation(delta) {
 }
 
 function start(event) {
-	alert("start1: " + event.targetTouches[0].pageY);
 	yStart = event.targetTouches[0].pageY;
 	elem.addEventListener("touchmove", move);
 }
 
 function move(event) {
-	alert("move1: " + event.targetTouches[0].pageY);
 	yMove = event.targetTouches[0].pageY;
 	var del = yStart - yMove;
-	if(del > 0) 
-	   {alert("up");}
-	else {alert("down");}
 
   scrollAnimation(del);
 
