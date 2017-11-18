79484397f3c93ad51dc2648e55a9c0247b197c89
Nazriy <Kereya18@gmail.com>
2017-11-18 12:30:05 +0200

    Update anim_script.js

--- Proccess/js/anim_script.js
+++ Proccess/js/anim_script.js
@@ -1,4 +1,27 @@
-alert("12.26");
+alert("12.29");
+
+var yStart = null;
+var delta = null;
+var yMove = null;
+var elem = document.getElementById('block_animation');
+
+elem.addEventListener("touchstart", start);
+//window.addEventListener("touchmove", move);
+	
+function start(event) {
+	alert("start1: " + event.targetTouches[0].pageY);
+	yStart = event.targetTouches[0].pageY;
+	elem.addEventListener("touchmove", move);
+}
+
+function move(event) {
+	alert("move1: " + event.targetTouches[0].pageY);
+	yMove = event.targetTouches[0].pageY;
+	delta = yStart - yMove;
+	if(delta > 0) 
+	   {alert("up");}
+	else {alert("down");}
+}
 
 var svg = document.getElementById('Process'),
     svgContainer = document.getElementById('svg_container'),
