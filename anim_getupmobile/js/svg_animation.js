4acbcb39cf7269a962f26a50f3467f304de6088d
Nazariy <Kereya18@gmail.com>
2017-12-15 11:58:58 +0200

    Update svg_animation.js

--- anim_getupmobile/js/svg_animation.js
+++ anim_getupmobile/js/svg_animation.js
@@ -2,8 +2,17 @@ window.onscroll = function() {startAnimation()};
 
 function startAnimation() {
     if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
-        document.getElementById("getupmobile").style.display = 'block';
+
+    	document.getElementById("getupmobile").style.display = 'block';
+    	document.querySelector(".vertical.flip-container .flipper").style.MozTransform = 'rotateX(-180deg)';
+    	document.querySelector(".vertical.flip-container .flipper").style.WebkitTransform = 'rotateX(-180deg)';
+    	document.querySelector(".vertical.flip-container .flipper").style.transform = 'rotateX(-180deg)';
+
     } else {
-        document.getElementById("getupmobile").style.display = 'none';
+      document.querySelector(".vertical.flip-container .flipper").style.MozTransform = 'rotateX(0deg)';
+    	document.querySelector(".vertical.flip-container .flipper").style.WebkitTransform = 'rotateX(0deg)';
+    	document.querySelector(".vertical.flip-container .flipper").style.transform = 'rotateX(0deg)';
+
+    	document.getElementById("getupmobile").style.display = 'none';
     }
 }
